10d9d0d5eca32ef533500b34a9f341b09e5e62ac
Aneerban Saha <98979613+rishii100@users.noreply.github.com>
2024-03-22 07:53:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,20 @@ def get_gemini_response(input,pdf_cotent,prompt):
 
 def input_pdf_setup(uploaded_file):
     if uploaded_file is not None:
-        images=pdf2image.convert_from_bytes(uploaded_file.read()
+        ## Convert the PDF to image
+        images=pdf2image.convert_from_bytes(uploaded_file.read())
+
         first_page=images[0]
+
+        # Convert to bytes
         img_byte_arr = io.BytesIO()
         first_page.save(img_byte_arr, format='JPEG')
         img_byte_arr = img_byte_arr.getvalue()
+
         pdf_parts = [
             {
                 "mime_type": "image/jpeg",
-                "data": base64.b64encode(img_byte_arr).decode()
+                "data": base64.b64encode(img_byte_arr).decode()  # encode to base64
             }
         ]
         return pdf_parts
@@ -46,6 +51,9 @@ if uploaded_file is not None:
 
 
 submit1 = st.button("Tell Me About the Resume")
+
+#submit2 = st.button("How Can I Improvise my Skills")
+
 submit3 = st.button("Percentage match")
 
 input_prompt1 = """
